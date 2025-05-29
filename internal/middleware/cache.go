249edf7f76e6dc18d543f30f7d881761d39a8acf
Ahmad Ridho <ahmad.ridho@gmail.com>
2025-05-29 09:45:24 +0700

    chore(cache): add companyId for cacheKey and update it to 1 min

--- internal/middleware/cache.go
+++ internal/middleware/cache.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,10 +14,21 @@ import (
 // - bypass with ?refresh=true
 func Cache() fiber.Handler {
 	return fibercache.New(fibercache.Config{
-		Expiration:   5 * time.Minute,
+		Expiration:   1 * time.Minute,
 		CacheControl: true,
+		// Optional manual bypass
 		Next: func(c *fiber.Ctx) bool {
 			return c.Query("refresh") == "true"
+		},
+
+		// Key includes companyId from token
+		KeyGenerator: func(c *fiber.Ctx) string {
+			companyId, ok := c.Locals("companyId").(string)
+			if !ok || companyId == "" {
+				// fallback to unauthenticated cache (or avoid caching)
+				return "cache:unknown:" + c.OriginalURL()
+			}
+			return fmt.Sprintf("cache:%s:%s", companyId, c.OriginalURL())
 		},
 	})
 }
